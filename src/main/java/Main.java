d74cf80da6a23bbe521236e9f6d38606d6badb43
riccardo <56031732+riccardolobue98@users.noreply.github.com>
2019-10-14 20:07:05 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,42 +8,269 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     	
-        String name = null;
-        String surname = null;
-        String mail = null;
-        String password = null;
-        String name_race = null;
-        String name_course = null;            
+        Scanner scan = new Scanner(System.in);
+    	
+        /**
+         * 
+         * creo nuovi oggeti di ti persona e ne dichiaro i valori
+         *
+         */
 
         Person person1 = new Person("Mattia", "Ricci", "mattiaricci@email.it", "password");
         Person person2 = new Person("Riccardo", "Lo Bue", "riccardolb@email.it", "password");
-    	
-       Person person3 = new Person( name , surname , mail , password );
-    	Person person4 = new Person( name , surname , mail , password );
+        
+        /**
+         * 
+         * creo un nuovo oggetto di ti attivita e ne dichiaro il nome
+         *
+         */
 
         Activity activity1 = new Activity("Sport");
+        
+        /**
+         * 
+         * creo un nuovo oggetto di ti race e ne dichiaro il nome
+         *
+         */
+        
         Race race1 = new Race("Formula 1");
+        
+        /**
+         * 
+         * creo un nuovo oggetto di ti course e ne dichiaro il nome
+         *
+         */
+        
         Course course1 = new Course(" Serie A");
         
-        Race race2 = new Race(name_race);
-        Course course2 = new Course(name_course);
+        /**
+         * 
+         * creo nuovi oggeti di tipo Member e Admin  e ne dichiaro i valori
+         *
+         */
         
         Member member1 = new Member("Giovanni", "Bianchi", "jbianchi@email.it", "password");
         Admin admin1 = new Admin("Peter", "Logan", "plogan@email.it",  "admin");
-
-        Member member2 = new Member("Giovanni", "Bianchi", "jbianchi@email.it", "password");
-        Admin admin2 = new Admin("Peter", "Logan", "plogan@email.it",  "admin");
+        
+        /**
+         * 
+         * visualizzo le Persone inizialmente inserite
+         *
+         */
 
         System.out.println("Every person existent:\n" + person1.toString() + "\n" +  person2.toString() + "\n");
+        
+		System.out.println("----------------------------------------------------------------------------------------");
+        
+        /**
+         * 
+         * Richiamo i metodi per aggiungere la prima e la seconda persona tramite l'oggetto activity1
+         *
+         */
         
         activity1.addPerson(person1);
         
         activity1.addPerson(person2);
         
+		System.out.println("USERS ADD");
+		
+		System.out.println("----------------------------------------------------------------------------------------");
+        
+        /**
+         * 
+         * Richiamo i metodi per eliminare una persona tramite l'oggetto di tipo activity1
+         *
+         */
+        
+        activity1.deletePerson(person2);
+		
+		System.out.println("USERS DELETE");
+		
+		System.out.println("----------------------------------------------------------------------------------------");
+            
+        /**
+         * 
+         * Richiamo i metodi per registrare una persona tramite l'oggetto di tipo admin1
+         *
+         */
+        
+        admin1.subscribeActivity(activity1);
+        
+		System.out.println("USER ADD");
+		
+		System.out.println("----------------------------------------------------------------------------------------");
+        
+        /**
+         * 
+         * Richiamo i metodi per registrare una persona tramite l'oggetto di tipo member1
+         *
+         */
+        
+        member1.subscribeActivity(activity1);
+        
+		System.out.println("USER ADD");
+		
+		System.out.println("----------------------------------------------------------------------------------------");
+        
+        /**
+         * 
+         * Richiamo i metodi per cancellare una persona dalla lista tramite l'oggetto di tipo member1
+         *
+         */
+        
+        admin1.unsubscribeActivity(activity1);
+        
+		System.out.println("USER DELETE");
+		
+		System.out.println("----------------------------------------------------------------------------------------");
+        
+        /**
+         * 
+         * Richiamo i metodi per cancellare una persona dalla lista tramite l'oggetto di tipo member1
+         *
+         */
+        
+        member1.unsubscribeActivity(activity1);
+        
+		System.out.println("USER DELETE");
+		
+		System.out.println("----------------------------------------------------------------------------------------");
+        
+        /**
+         * 
+         * Richiamo i metodi per la modifica di una persona tramite l'oggetto di tipo member1
+         *
+         */
+        
+        String name;
+		String surname;
+		String mail;
+		String password;
+		
+		/*
+		
+		System.out.println("INSERT Name,Surname,Mail and Password");
+    	
+    	name= scan.next();
+    	
+    	surname= scan.next();
+    	
+    	mail= scan.next();
+    	
+    	password= scan.next();
+    	
+    	*/
+        
+        admin1.ModifyAdmin(admin1);
+        
+		System.out.println("MODIFY COMPLETE");
+        
+		System.out.println("----------------------------------------------------------------------------------------");
+		
+        /**
+         * 
+         * Richiamo i metodi per la modifica di una persona tramite l'oggetto di tipo admin1
+         *
+         */
+		
+		System.out.println("INSERT Name,Surname,Mail and Password");
+    	
+    	name= scan.next();
+    	
+    	surname= scan.next();
+    	
+    	mail= scan.next();
+    	
+    	password= scan.next();
+		
+		member1.modifyData(member1, name, surname, mail, password);
+		
+		System.out.println("MODIFY COMPLETE");
+        	
+		System.out.println("----------------------------------------------------------------------------------------");
+		
+        /**
+         * 
+         * Richiamo i metodi per la modifica di una persona tramite l'oggetto di tipo member1
+         *
+         */
+		
+		System.out.println("INSERT Name,Surname,Mail and Password");
+    	
+    	name= scan.next();
+    	
+    	surname= scan.next();
+    	
+    	mail= scan.next();
+    	
+    	password= scan.next();
+		
+		member1.modifyData(member1, name, surname, mail, password);
+		
+		System.out.println("MODIFY COMPLETE");
+		
+        /**
+         * 
+         * Faccio un ciclo per ogni scritto li visualizzo su console tramite l'oggetto di tipo activity1
+         *
+         */
+		
+		System.out.println("----------------------------------------------------------------------------------------");
+		
+		for (Person p:activity1.getSubscribers()) {
+			
+            System.out.println("Ecco gli iscritti all'attivita scelta: " + p.toString() + "\n");
+            
+        }
+		
+		System.out.println("----------------------------------------------------------------------------------------");
+
+        /**
+         * 
+         * visualizzo il membro su console tramite l'oggetto di tipo member1
+         *
+         */
+		
+        System.out.println("Every member existent: " + member1.toString() + "\n");
+        
+		System.out.println("----------------------------------------------------------------------------------------");
+		
+        /**
+         * 
+         * visualizzo il membro su console tramite l'oggetto di tipo admin1
+         *
+         */
+		
+        System.out.println("Every member existent: " + admin1.toString() + "\n");
+        
+		System.out.println("----------------------------------------------------------------------------------------");
+		
+        /**
+         * 
+         * Faccio un ciclo che per ogni scritto alla gara o al corso li visualizzo su console tramite l'oggetto di tipo race1 e course1
+         *
+         */
+		
+        for (Person p:race1.getSubscribers()) {
+        	
+            System.out.println("Every subscribers to the race: " + p.toString() + "\n");
+            
+        }
+
+		System.out.println("----------------------------------------------------------------------------------------");
+        
+        for(Person p:course1.getSubscribers()) {
+        	
+            System.out.println("Every subscribers to the course: " + p.toString() + "\n");
+            
+        }
+        
+
 //////////////////////////////////////////////////////////////////////////////////////////////
         
-        Scanner scan = new Scanner(System.in);
-        
+        /*
+         * 
+
         int response;
         
         int conditions = 1;
@@ -317,28 +544,37 @@ public class Main {
         
         System.out.println("Grazie ed arrivederci");
         
+        */
+        
 //////////////////////////////////////////////////////////////////////////////////////////////
 
         //activity1.deletePerson(person1);
 
         /*for (Person p:activity1.getSubscribers()) {
+         * 
             System.out.println("Ecco gli iscritti all'attivita scelta: " + p.toString() + "\n");
+            
         }
 
         member1.subscribeActivity(activity1);
+        
         member1.subscribeActivity(race1);
 
         admin1.subscribeActivity(activity1);
+        
         admin1.subscribeActivity(race1);
 
         System.out.println("Every member existent: " + member1.toString() + "\n");
+        
         System.out.println("Every member existent: " + admin1.toString() + "\n");
 
         for (Person p:race1.getSubscribers()) {
+        
             System.out.println("Every subscribers to the race: " + p.toString() + "\n");
         }
 
         for(Person p:course1.getSubscribers()) {
+        
             System.out.println("Every subscribers to the course: " + p.toString() + "\n");
         }
         
