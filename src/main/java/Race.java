73821d553b71edddc6ce0465a59a2656dd58d07a
riccardo <56031732+riccardolobue98@users.noreply.github.com>
2019-10-14 13:10:22 +0200

    Update Race.java

--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -2,7 +2,7 @@
  * Race is an Activity subclass
  *
  * @author Mattia Ricci mattia.ricci1@studenti.unipr.it
- * @author Riccardo Lo Bue riccardo.lobue@studenti.unipr.it
+ * @author Gaspare Riccardo Lo Bue gasparericcardo.lobue@studenti.unipr.it
  */
 public class Race extends Activity {
 
